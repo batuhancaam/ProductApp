9a2a2a8d9d2e7534d66cca405aa978fe5a928904
Batuhan Çam <batuc06@gmail.com>
2019-05-21 22:22:05 +0300

    Components added

---
+++ src/components/Product.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-md-2 card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Product",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
